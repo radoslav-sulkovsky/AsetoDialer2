60ddc09855b8675588e41818f77afa27980f2a39
radoslav-sulkovsky <radoslav.sulkovsky@gmail.com>
2018-03-28 14:28:28 +0200

    SmsReceiver deleted commented code

--- app/src/main/java/aseto/dialer/SmsReceiver.java
+++ app/src/main/java/aseto/dialer/SmsReceiver.java
@@ -3,8 +3,6 @@ package aseto.dialer;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
-import android.os.Bundle;
 import android.provider.Telephony;
 import android.telephony.SmsMessage;
 
@@ -18,33 +16,15 @@ public class SmsReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)) {
+        if(intent.getAction().equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)) {
 
             String smsSender = "";
             String smsBody = "";
 
-            /* if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) { */
-
-            for (SmsMessage smsMessage : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
+            for(SmsMessage smsMessage : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
                 smsSender = smsMessage.getDisplayOriginatingAddress();
                 smsBody += smsMessage.getMessageBody();
             }
-            
-            /* } else {
-                Bundle smsBundle = intent.getExtras();
-                if (smsBundle != null) {
-                    Object[] pdus = (Object[]) smsBundle.get("pdus");
-                    if (pdus == null) {
-                        return;
-                    }
-                    SmsMessage[] messages = new SmsMessage[pdus.length];
-                    for (int i = 0; i < messages.length; i++) {
-                        messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
-                        smsBody += messages[i].getMessageBody();
-                    }
-                    smsSender = messages[0].getOriginatingAddress();
-                }
-            } */
 
             Events.newSms(smsSender, smsBody, Calendar.getInstance().getTime());
         }
